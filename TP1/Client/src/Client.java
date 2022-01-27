e1525767a79d755d6abbf1af820647a7442a44c4
RayanBouteldja <70912595+RayanBouteldja@users.noreply.github.com>
2022-01-27 01:34:24 -0500

    ecriture code client

--- TP1/Client/src/Client.java
+++ TP1/Client/src/Client.java
@@ -1,6 +1,31 @@
+import java.io.DataInputStream;
+import java.net.Socket;
 
-public class Client {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+public class Client
+{
+	private static Socket socket;
+	/* Application client
+	 */
+	public static void main(String[] args) throws Exception
+	{
+		//Adresse et port du serveur
+		String serverAddress = "127.0.0.1";
+		int port = 5000;
+		
+		// Création d'une nouvelle connexion avec le serveur
+		socket = new Socket(serverAddress, port);
+		
+		System.out.format("The server is running on %s:%d%n", serverAddress, port);
+		
+		// Création d'un canal entrant pour recevoir les messages envoyés par le serveur
+		DataInputStream in = new DataInputStream(socket.getInputStream());
+		
+		// Attente de la réception d'un message envoyé par le serveur sur le canal
+		String helloMessageFromServer = in.readUTF();
+		System.out.println(helloMessageFromServer);
+		
+		// Fermeture de la connexion aves le serveur
+		socket.close();
 	}
 }
+
